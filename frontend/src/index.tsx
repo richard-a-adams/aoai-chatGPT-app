bd256c8083cf36bd4b6b1131b4d65e0b62efce12
Richard-Adams <richard.adams@segaamerica.com>
2024-05-03 00:56:06 +0000

    add app insights to index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,6 +9,13 @@ import Layout from "./pages/layout/Layout";
 import NoPage from "./pages/NoPage";
 import Chat from "./pages/chat/Chat";
 import { AppStateProvider } from "./state/AppProvider";
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: 'InstrumentationKey=c007b55e-bc8c-42f9-8174-d579b3accecb;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=28cd8376-e233-4809-b62b-25d9c33e0605'
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView();
 
 initializeIcons();
 
